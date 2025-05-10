72cb27a9371dea9f0d6aef57afe60036dcc5434c
Ryan Jeremy Pridgen <encodedrj@icloud.com>
2025-05-10 20:49:54 +0000

    fileshare

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -7,53 +7,52 @@ terraform {
   }
 }
 
-variable "account_id" {
-  description = "Cloudflare account ID"
-  type        = string
-  default = "02ee7b87eb8a52627a53b88821c9ae95"
+data "cloudflare_accounts" "this" {
+  name = "Siguiente"
+}
+
+resource "cloudflare_zero_trust_list" "apple_software" {
+  name = "Apple Software Updates"
+  type = "DOMAIN"
+  account_id = data.cloudflare_accounts.this.id
+  items = [
+    { value: "appldnld.apple.com" },
+    { value: "configuration.apple.com" },
+    { value: "gdmf.apple.com" },
+    { value: "gg.apple.com" },
+    { value: "gs.apple.com" },
+    { value: "ig.apple.com" },
+    { value: "mesu.apple.com" },
+    { value: "oscdn.apple.com" },
+    { value: "osrecovery.apple.com" },
+    { value: "skl.apple.com" },
+    { value: "swcdn.apple.com" },
+    { value: "swdist.apple.com" },
+    { value: "swdownload.apple.com" },
+    { value: "swscan.apple.com" },
+    { value: "updates-http.cdn-apple.com" },
+    { value: "updates.cdn-apple.com" },
+    { value: "xp.apple.com" },
+    { value: "gdmf-ados.apple.com" },
+    { value: "gsra.apple.com" },
+    { value: "wkms-public.apple.com" },
+    { value: "fcs-keys-pub-prod.cdn-apple.com" }
+  ]
 }
 
 resource "cloudflare_zero_trust_gateway_policy" "block_filesharing" {
-  account_id = var.account_id
-  action = "block"
-  filters = ["dns"]
+  account_id = data.cloudflare_accounts.this.id
+  action = "allow"
+  filters = ["http"]
   name = "Filesharing"
   description = "Block all filesharing services for system integrity and security."
   enabled = true
-  traffic = "any(dns.content.category[*] in {95})"
-  identity = ""
-  precedence = 10
-  rule_settings = {
-    block_page_enabled = false
-    ip_categories = true
-    ip_indicator_feeds = true
-    insecure_disabled_dnssec_validation = false
-  }
-}
-
-resource "cloudflare_zero_trust_gateway_policy" "isolate" {
-  account_id = var.account_id
-  action = "isolate"
-  filters = ["http"]
-  name = "Isolation"
-  description = "Browser isolation policy for all HTTP browser traffic."
-  enabled = true
-  precedence = 10000
   traffic = "http.request.method in {\"GET\" \"POST\" \"PUT\" \"DELETE\" \"PATCH\"}"
-  rule_settings = {
-    biso_admin_controls = {
-      copy = "remote_only"
-      dcp = false
-      dd = false
-      dk = false
-      download = "enabled"
-      dp = false
-      du = false
-      keyboard = "enabled"
-      paste = "enabled"
-      printing = "enabled"
-      upload = "enabled"
-      version = "v1"
+  precedence = 10
+  rule_settings {
+    add_headers = {
+      "HELP" = "ryan jeremy pridgen"
+      "HELP_GEO" = "20.50694,-86.94847"
     }
   }
 }
