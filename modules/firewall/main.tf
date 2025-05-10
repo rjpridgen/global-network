879cb2575674cddd0f9daa84599a8947692e0e66
Ryan Jeremy Pridgen <encodedrj@icloud.com>
2025-05-10 07:44:30 -0700

    Block filesharing (dropbox, etc)

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -13,6 +13,24 @@ variable "account_id" {
   default = "02ee7b87eb8a52627a53b88821c9ae95"
 }
 
+resource "cloudflare_zero_trust_gateway_policy" "block_filesharing" {
+  account_id = var.account_id
+  action = "block"
+  filters = ["dns"]
+  name = "Filesharing"
+  description = "Block all filesharing services for system integrity and security."
+  enabled = true
+  filter = "any(dns.content.category[*] in {95})"
+  identity = ""
+  precedence = 10
+  rule_settings = {
+    block_page_enabled = false
+    ip_categories = true
+    ip_indicator_feeds = true
+    insecure_disabled_dnssec_validation = false
+  }
+}
+
 resource "cloudflare_zero_trust_gateway_policy" "isolate" {
   account_id = var.account_id
   action = "isolate"
