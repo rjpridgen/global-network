16922e51ed2770d6cb9bc12c41b51b6c0d8697a1
rjpridgen <rjpridgen@icloud.com>
2025-05-12 15:40:15 -0700

    cloudflare

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = ">= 1.11.4"
 }
 
+module "zero-trust-firewall" {
+  source = "./modules/firewall"
+  team = "ryanjeremypridgen"
+}
+
 module "global-network" {
   source = "./modules/network"
   google_project_id = "siguiente-mexico"
