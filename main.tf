4058becc530074f023c8181bbb9734fa3134b620
rjpridgen <rjpridgen@icloud.com>
2025-05-12 04:43:04 -0700

    google provider

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google-beta"
+      version = "6.34.0"
+    }
+  }
   required_version = ">= 1.11.4"
+}
+
+provider "google" {
+  project = "siguiente-mexico"
+  region = "northamerica-south1"
 }
 
 module "firewall" {
