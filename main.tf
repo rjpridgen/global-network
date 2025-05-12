9eb43389d2b2f8b71f22d4a462360eae083494e9
rjpridgen <rjpridgen@icloud.com>
2025-05-12 04:45:07 -0700

    global compute network

--- main.tf
+++ main.tf
@@ -13,6 +13,13 @@ provider "google" {
   region = "northamerica-south1"
 }
 
+resource "google_compute_network" "vpc_siguiente_global" {
+  name                    = "vpc-siguiente-global"
+  auto_create_subnetworks = true
+  routing_mode            = "GLOBAL"
+  mtu                     = 1460
+}
+
 module "firewall" {
   source = "./modules/firewall"
 }
