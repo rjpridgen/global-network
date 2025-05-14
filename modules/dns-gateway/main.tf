15425186ad9d58f7a6cb4c5660dcde4826f7ed68
Ryan Jeremy Pridgen <encodedrj@icloud.com>
2025-05-14 13:23:07 +0000

    disable org

--- modules/dns-gateway/main.tf
+++ modules/dns-gateway/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     cloudflare = {
-        source = "cloudflare/cloudflare"
+      source = "cloudflare/cloudflare"
     }
   }
 }
@@ -11,12 +11,14 @@ locals {
 }
 
 resource "cloudflare_zero_trust_gateway_policy" "domain_block" {
-  account_id = var.account
-  action = "block"
-  name = "Domain Block"
+  account_id  = var.account
+  action      = "block"
+  name        = "Domain Block"
   description = "Block bad websites based on their host name."
-  enabled = true
-  filters = ["dns"]
-  precedence = 0
-  traffic = "any(dns.domains[*] in {${local.block_lists}})"
+  enabled     = true
+  filters     = ["dns"]
+  precedence  = 0
+  traffic = join(" or ", [
+    for id in local.block_lists : "any(dns.domains[*] in {${id}})"
+  ])
 }
