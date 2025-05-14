e00efbf30d416096c8e5cb3d439b30e5f13257bb
BF <bf@Ryans-MacBook-Air.local>
2025-05-14 00:56:55 -0700

    amazon

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -36,100 +36,19 @@ data "http" "amazon_web_services" {
 }
 
 locals {
-  aws_data = [ for response in data.http.amazon_web_services : jsondecode(response.body).ServiceDetails ]
-
-  amazon_web_services_responses = [
-     for response in local.aws_data : {
-				ipv4 = flatten([for service in response : service.PrivateIpv4DnsNames])
-     }
-   ]
-
-  github_api_metadata = jsondecode(data.http.github_meta.body)
+  services = [ for response in data.http.amazon_web_services : jsondecode(response.body).ServiceDetails ]
+  ipv4 = flatten([ for service in local.services : service.PrivateIpv4DnsNames if can(service.PrivateIpv4DnsNames) ])
 }
 
-resource "cloudflare_zero_trust_list" "github" {
-  for_each = toset(["web", "api", "git", "packages", "pages", "importer", "codespaces", "copilot"])
-  name = "Github ${each.value}"
-  type = "IP"
-  account_id = var.account
-  items = [
-    for ip in local.github_api_metadata[each.value]: {
-      value = ip
-    }
-  ]
-}
-
-
-locals {
-  cloudflare_hostnames = [
-    "cloudflare.com",
-    "dash.cloudflare.com",
-    "one.dash.cloudflare.com",
-    "cloudflare-dns.com",
-    "engage.cloudflareclient.com",
-    "zero-trust-client.cloudflareclient.com",
-    "connectivity.cloudflareclient.com",
-    "time.cloudflare.com",
-    "${var.team}.cloudflareaccess.com",
-    "${var.team}.cloudflare-gateway.com"
-  ]
-
-  cloudflare_ip = [
-    ## WARP Firewall ##
-    ## https://developers.cloudflare.com/cloudflare-one/connections/connect-devices/warp/deployment/firewall/ ##
-
-    # WARP Client Orchestration API
-    "162.159.137.105",
-    "162.159.138.105",
-    "2606:4700:7::a29f:8969",
-    "2606:4700:7::a29f:8a69",
-
-    # DOH IP
-    "162.159.36.1",
-    "162.159.46.1",
-    "2606:4700:4700::1111",
-    "2606:4700:4700::1001",
-
-    # WARP Ingress WireGuard
-    "162.159.193.0/24",
-    "2606:4700:100::/48",
-
-    # WARP Ingress MASQUE
-    "162.159.197.0/24",
-    "2606:4700:102::/48",
-
-    # Engage
-    "162.159.197.3",
-    "2606:4700:102::3",
-
-    # WARP Client Connectivity
-    "162.159.197.4",
-    "2606:4700:102::4"
-  ]
-}
-
-data "cloudflare_accounts" "this" {
-  name = "Siguiente"
-}
-
-resource "cloudflare_zero_trust_list" "cloudflare" {
-  name = "Cloudflare Hostnames"
+resource "cloudflare_zero_trust_list" "amazon_ipv4" {
+  for_each = toset(local.ipv4)
+  name = "Amazon IPv4"
+  description = ""
+  account_id = ""
   type = "DOMAIN"
-  account_id = var.account
   items = [
-    for s in local.cloudflare_hostnames : {
-      value = s
-    }
-  ]
-}
-
-resource "cloudflare_zero_trust_list" "cloudflare_ip" {
-  name = "Cloudflare IP"
-  type = "IP"
-  account_id = var.account
-  items = [
-    for s in local.cloudflare_ip : {
-      value = s
+    for s in local.ipv4 : {
+      value = tostring(s)
     }
   ]
 }
