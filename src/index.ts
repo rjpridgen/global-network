3387a39a2f7343beb2731668b7d8abbdbc828962
Ryan Jeremy Pridgen <encodedrj@icloud.com>
2025-05-14 14:25:08 +0000

    reset, org

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import { Cloudflare } from "cloudflare"
 
+function error(message: string) {
+  return (error) => console.error(message, error)
+}
+
 const api = new Cloudflare({
-    apiKey: process.env.CLOUDFLARE_API_KEY,
-    apiEmail: process.env.CLOUDFLARE_EMAIL,
+    apiToken: process.env.CLOUDFLARE_API_TOKEN
 })
 
 const acc = (await api.accounts.list()).result.at(0)!
 
-for await (const gatewayRule of api.zeroTrust.gateway.rules.list({
+for await (const app of api.zeroTrust.access.applications.list({
   account_id: acc.id
 })) {
-  console.log(gatewayRule);
+  await api.zeroTrust.access.applications.delete(app.id, {
+    account_id: acc.id
+  }).catch(error("Error deleting application."))
+}
+
+for await (const policy of api.zeroTrust.access.policies.list({
+  account_id: acc.id
+})) {
+  await api.zeroTrust.access.policies.delete(policy.id, {
+    account_id: acc.id
+  }).catch(error("Error deleting policy."))
+}
+
+for await (const list of api.zeroTrust.gateway.lists.list({
+  account_id: acc.id
+})) {
+  await api.zeroTrust.gateway.lists.delete(list.id, {
+    account_id: acc.id
+  }).catch(error("Error deleting list"))
 }
