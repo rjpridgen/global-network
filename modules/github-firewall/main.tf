2941541adc4d22f6493890ac69146eac3bbf3b93
Ryan Jeremy Pridgen <encodedrj@icloud.com>
2025-05-16 04:00:24 +0000

    fix apply

--- modules/github-firewall/main.tf
+++ modules/github-firewall/main.tf
@@ -15,16 +15,15 @@ data "http" "this" {
 
 locals {
     address_list = jsondecode(data.http.this.response_body)
-    cidr = [
-        for address in local.address_list[var.category] : {
-            value = address
-        }
-    ]
 }
 
 resource "cloudflare_zero_trust_list" "this" {
     account_id = var.account_id
     name = "Github (${var.category})"
     type = "IP"
-    items = local.cidr
+    items = [
+        for address in local.address_list[var.category] : {
+            value = address
+        }
+    ]
 }
